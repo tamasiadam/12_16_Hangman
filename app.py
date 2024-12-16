84956eb5fb34b37999f4b64669be5d475f15b47f
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2024-12-16 09:23:20 +0100

    elif update

--- app.py
+++ app.py
@@ -7,23 +7,29 @@ print("Üdvözöllek az akasztófa játékban!")
         
 
 def easy():
-    orszagok = random.randint(countries)
-    print(orszagok)
+    orszag_index = random.randint(0, 182)
+    orszag = countries[orszag_index]
+    print(orszag_index)
     
-def kezdes():
+    tipp = input("Adj meg egy betűt, vagy ")
+    if tipp == orszag:
+        print("Grat nyertél")
+    
 
+def kezdes():
     while True:
         jatek_valasztas = int(input("Válassz szintet!\nKönnyű (1)\nKözepes (2)\nNehéz (3)\nVálassz!: "))
         if jatek_valasztas == 1:
             print("Könnyű nehézség kiválasztva! ✅")
             easy()
-        if jatek_valasztas == 2:
+        elif jatek_valasztas == 2:
             print("Közepes nehézség kiválasztva! ✅")
             medium()
-        if jatek_valasztas == 3:
+        elif jatek_valasztas == 3:
             print("Nehéz nehézség kiválasztva! ✅")
             hard()
         else:
             print("Helytelen formátum! ❌")
+            
 
 kezdes()
