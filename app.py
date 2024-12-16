209f1cfd42a47f6bb7eff471d90054762852799a
tamasiadam <tamasi.adam.attila@diak.szbi-pg.hu>
2024-12-16 22:54:43 +0100

    Azonos válaszok, sorok rendezése, hiányok feltűntetése

--- app.py
+++ app.py
@@ -3,26 +3,23 @@ import random
 
 countries = get_countries()
 
-#alsóvonalak különválasztása külön szavaknál
-
-
 print("Üdvözöllek az Akasztófa játékban!")
-        
+
+orszag_index = random.randint(0, 182) #182 vagy 183?
+orszag = countries[orszag_index]
+orszag_len = len(orszag)
+ismeretlen = ""
+for i in orszag:
+    if i == " ":
+        ismeretlen += "   "
+    else:
+        ismeretlen += "_ "
+jo_tippek = []
+rossz_tippek = []
+
 
 def easy():
     life = 7
-    jo_tippek = []
-    rossz_tippek = []
-
-    orszag_index = random.randint(0, 182) #182 vagy 183?
-    orszag = countries[orszag_index]
-    orszag_len = len(orszag)
-    ismeretlen = ""
-    for i in orszag:
-        if i == " ":
-            ismeretlen += "   "
-        else:
-            ismeretlen += "_ "
 
     print(f"Az ország: {orszag}")
     print(f"Hossz: {orszag_len}")
@@ -35,29 +32,36 @@ def easy():
         if tipp.lower() == orszag.lower():
             print("Gratulálok, nyertél! 🏆")
         
+        elif tipp.lower() in jo_tippek or rossz_tippek: #kis- és nagybetűk különbözőnek számítanak, ezt egyenlőként kéne kezelni
+            print("Ezt a betűt már próbáltad! ❌")
+        
         elif tipp.lower() in orszag.lower():
             jo_tippek.append(tipp)
-            print(f"Válaszod helyes, \nRossz válaszok: {rossz_tippek} \nJó válaszok: {jo_tippek}")
+            print(f"Helyes válasz! ✅\nRossz válaszok: {rossz_tippek} \nJó válaszok: {jo_tippek}")
+            #replace?
+            #megkeresni hol vannak azok a betűk, ott kicserélni pontosan, ha van többet is akár
             print(ismeretlen)
         
         elif tipp.lower() not in orszag.lower():
             rossz_tippek.append(tipp)
             life -= 1
-            print(f"Válaszod helytelen, \nRossz válaszok: {rossz_tippek} \n Jó válaszok: {jo_tippek}")
+            print(f"Helytelen válasz! ❌\n>Rossz válaszok: {rossz_tippek}\n>Jó válaszok: {jo_tippek}")
             print("Megmaradt életed:", life, " 💔")
-        0   
+
 def kezdes():
     while True:
         jatek_valasztas = int(input("Válassz szintet!\nKönnyű (1)\nKözepes (2)\nNehéz (3)\nVálassz!: "))
         if jatek_valasztas == 1:
             print("Könnyű nehézség kiválasztva! ✅")
             easy()
+
         elif jatek_valasztas == 2:
             print("Közepes nehézség kiválasztva! ✅")
             medium()
+            
         elif jatek_valasztas == 3:
             print("Nehéz nehézség kiválasztva! ✅")
-            hard()
+        
         else:
             print("Helytelen formátum! ❌")
 
