33514cdde6a0b3bd59e8bd68cbc98f2548e87af8
Ádám <tamasi.adam.attila@diak.szbi-pg.hu>
2024-12-16 08:34:01 +0100

    elso feltoltes

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 from country_list import get_countries
 
 countries = get_countries()
-print(countries)
+
+print("Üdvözöllek az akasztófa játékban!")
+
+
+def kezdes():
+
+    while True:
+        jatek_valasztas = int(input("Válassz szintet!\nKönnyű (1)\nKözepes (2)\nNehéz (3)\nVálassz!: "))
+        if jatek_valasztas == 1:
+            print("Könnyű nehézség kiválasztva! ✅")
+            easy()
+        if jatek_valasztas == 2:
+            print("Közepes nehézség kiválasztva! ✅")
+            medium()
+        if jatek_valasztas == 3:
+            print("Nehéz nehézség kiválasztva! ✅")
+            hard()
+        else:
+            print("Helytelen formátum! ❌")
+
