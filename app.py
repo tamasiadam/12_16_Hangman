497bbaf4e7bb9dfe5eb730636ba4f4858b170c64
tamasiadam <tamasi.adam.attila@diak.szbi-pg.hu>
2025-01-19 19:35:18 +0100

    Végleges hangman
    Hangman by LÁB 1.0

--- app.py
+++ app.py
@@ -11,127 +11,32 @@ print("Üdvözöllek az Akasztófa játékban!")
 jo_tippek = []
 rossz_tippek = []
 
-#Könnyű mód: 7 élet, 1 szavas országok
-def easy():
-    orszag = random.choice(countries)
-    
-    while " " in orszag:
-        orszag = random.choice(countries)
-    
-    orszag_len = len(orszag)
-    ismeretlen = ""
-    
-    for i in orszag:
-        if i == " ":
-            ismeretlen += "   "
-        else:
-            ismeretlen += "_ "
+# nehezseg: 0 -> könnyű, 1 -> közepes, 2 -> nehéz
+def jatek(nehezseg):
+    jo_tippek.clear()
+    rossz_tippek.clear()
 
+    #életek száma
     elet = 7
 
-    print(orszag)
-    print(f"Az ország hossza: {orszag_len} karakter.")
-    print(f"A kitalálandó ország: \n{ismeretlen}")
+    match(nehezseg):
+        case 0: 
+            orszag = random.choice([country for country in countries if " " not in country]) # minden ország amiben nincs szóköz
+        case 1: 
+            orszag = random.choice([country for country in countries if " " in country]) # minden ország amiben van szóköz
+        case 2: 
+            elet = 5
+            orszag = random.choice(capitals)
 
-    while elet > 0:
-        tipp = input("Adj meg egy betűt, vagy megoldást: ").strip()
-
-        if tipp.lower() == orszag.lower():
-            print("Gratulálok, kitaláltad az országot! 🎉")
-            while True:
-                ask = input("Szeretnél még játszani? (Igen/Nem)")
-                if ask.lower() == "igen":
-                    jo_tippek.clear()
-                    rossz_tippek.clear()
-                    kezdes()
-                elif ask.lower() == "nem":
-                    print("Játék vége!")
-                    exit()
-                else:
-                    print("Ezt nem tudom értelmezni")
-
-        if "_" not in ismeretlen:
-            print("Gratulálok, kitaláltad az országot! 🎉")
-            
-            while True:
-                ask = input("Szeretnél még játszani? (Igen/Nem)")
-                if ask.lower() == "igen":
-                    jo_tippek.clear()
-                    rossz_tippek.clear()
-                    kezdes()
-                elif ask.lower() == "nem":
-                    print("Játék vége!")
-                    exit()
-                else:
-                    print("Ezt nem tudom értelmezni")
-
-        #már felhasznált betű
-        elif tipp.lower() in jo_tippek or tipp.lower() in rossz_tippek:
-            print("Ezt a betűt már próbáltad! ❌")
-
-        #kilépés
-        elif tipp.lower() == "quit":
-            kilepes = input("- Kilépés -\nBiztos ki akarsz lépni? (igen/nem): ").strip().lower()
-            if kilepes == "igen":
-                print("Kilépés..")
-                exit()
-            else:
-                print("Játék folytatása...")
-                continue
-
-        #helyes betű
-        elif tipp.lower() in orszag.lower():
-            jo_tippek.append(tipp.lower())
-            print(f"Helyes válasz! ✅\n")
-            print("Rossz válaszok:", ", ".join(rossz_tippek), "\nJó válaszok:", ", ".join(jo_tippek))
-
-            #megadott betűk kicserélése
-            uj_ismeretlen = ""
-            for i in range(len(orszag)):
-                if orszag[i].lower() == tipp.lower():
-                    uj_ismeretlen += orszag[i] + " "
-                else:
-                    uj_ismeretlen += ismeretlen[i * 2] + " "
-            ismeretlen = uj_ismeretlen
-            print(ismeretlen)
-
-        #rossz válasz
-        elif tipp.lower() not in orszag.lower():
-            rossz_tippek.append(tipp.lower())
-            elet -= 1
-            
-            print(HANGMANPICS[6 - elet])
-            print(f"Helytelen válasz! ❌>")
-            print("> Rossz válaszok:", ", ".join(rossz_tippek), "\n> Jó válaszok:", ", ".join(jo_tippek))
-            print("> Megmaradt életed:", elet, " 💔")
-
-            if elet == 0:
-                print(f"Vesztettél! Az ország: {orszag}")
-                
-                while True:
-                    ask = input("Szeretnéd újra megpróbálni? (Igen/Nem)")
-                    if ask.lower() == "igen":
-                        jo_tippek.clear()
-                        rossz_tippek.clear()
-                        break
-                    elif ask.lower() == "nem":
-                        print("Játék vége!")
-                        exit()
-                    else:
-                        print("Ezt nem tudom értelmezni! Igen vagy Nem választ fogadok el csak.")
-
-#Közepes mód: 7 élet, többszavas országok
-def medium():
-    orszag = random.choice(countries)
     orszag_len = len(orszag)
     ismeretlen = ""
     for i in orszag:
         if i == " ":
-            ismeretlen += "   "
+            ismeretlen += " "
+        elif i != "|":
+            ismeretlen += "_"
         else:
-            ismeretlen += "_ "
-
-    elet = 7
+            ismeretlen += "|"
 
     print(orszag)
     print(f"Az ország hossza: {orszag_len} karakter.")
@@ -147,35 +52,12 @@ def medium():
             while True:
                 ask = input("Szeretnél még játszani? (Igen/Nem)")
                 if ask.lower() == "igen":
-                    jo_tippek.clear()
-                    rossz_tippek.clear()
                     kezdes()
                 elif ask.lower() == "nem":
                     print("Játék vége!")
                     break
                 else:
-                    print("Ezt nem tudom értelmezni")
-
-        #Nincs több ismeretlen betű
-        if "_" not in ismeretlen:
-            print("Gratulálok, kitaláltad az országot! 🎉")
-            
-            while True:
-                ask = input("Szeretnél még játszani? (Igen/Nem)")
-                if ask.lower() == "igen":
-                    jo_tippek.clear()
-                    rossz_tippek.clear()
-                    kezdes()
-                elif ask.lower() == "nem":
-                    print("Játék vége!")
-                    exit()
-                else:
-                    print("Ezt nem tudom értelmezni")
-
-        #már felhasznált betű
-        elif tipp.lower() in jo_tippek or tipp.lower() in rossz_tippek:
-            print("Ezt a betűt már próbáltad! ❌")
-
+                    print("Ezt nem tudom értelmezni! Igen vagy Nem választ fogadok el csak.")
         #kilépés
         elif tipp.lower() == "quit":
             kilepes = input("- Kilépés -\nBiztos ki akarsz lépni? (igen/nem): ").strip().lower()
@@ -185,6 +67,17 @@ def medium():
             else:
                 print("Játék folytatása...")
                 continue
+        #Rossz tipp
+        elif len(tipp) != 1:
+            elet -= 1
+            print(HANGMANPICS[6 - elet])
+            print(f"Helytelen válasz! ❌")
+            print("> Rossz válaszok:", ", ".join(rossz_tippek), "\n> Jó válaszok:", ", ".join(jo_tippek))
+            print("> Megmaradt életed:", elet, " 💔")
+
+        #már felhasznált betű
+        elif tipp.lower() in jo_tippek or tipp.lower() in rossz_tippek:
+            print("Ezt a betűt már próbáltad! ❌")
 
         #helyes betű
         elif tipp.lower() in orszag.lower():
@@ -196,121 +89,26 @@ def medium():
             uj_ismeretlen = ""
             for i in range(len(orszag)):
                 if orszag[i].lower() == tipp.lower():
-                    uj_ismeretlen += orszag[i] + " "
+                    uj_ismeretlen += orszag[i]
                 else:
-                    uj_ismeretlen += ismeretlen[i * 2] + " "
+                    uj_ismeretlen += ismeretlen[i]
             ismeretlen = uj_ismeretlen
             print(ismeretlen)
 
-        #rossz válasz
-        elif tipp.lower() not in orszag.lower():
-            rossz_tippek.append(tipp.lower())
-            elet -= 1
-            
-            print(HANGMANPICS[6 - elet])
-            print(f"Helytelen válasz! ❌")
-            print("> Rossz válaszok:", ", ".join(rossz_tippek), "\n> Jó válaszok:", ", ".join(jo_tippek))
-            print("> Megmaradt életed:", elet, " 💔")
-
-            if elet == 0:
-                print(f"Vesztettél! Az ország: {orszag}")
-
+            #Nincs több ismeretlen betű
+            if "_" not in ismeretlen:
+                print("Gratulálok, kitaláltad az országot! 🎉")
+                
                 while True:
-                    ask = input("Szeretnéd újra megpróbálni? (Igen/Nem)")
+                    ask = input("Szeretnél még játszani? (Igen/Nem)")
                     if ask.lower() == "igen":
-                        jo_tippek.clear()
-                        rossz_tippek.clear()
-                        break
+                        kezdes()
                     elif ask.lower() == "nem":
                         print("Játék vége!")
                         exit()
                     else:
                         print("Ezt nem tudom értelmezni! Igen vagy Nem választ fogadok el csak.")
 
-#Nehéz mód: 5 élet, többszavas ország + fővárosa
-def hard():
-    orszag = random.choice(countries)
-    orszag_len = len(orszag)
-    fovaros = random.choice(capitals)
-    fovaros_len = len(fovaros)
-    ismeretlen = ""
-    for i in orszag:
-        if i == " ":
-            ismeretlen += "   "
-        elif i == "|":
-            ismeretlen += "| "
-        else:
-            ismeretlen += "_ "
-
-    elet = 5
-
-    print(orszag)
-    print(f"Az ország hossza: {orszag_len} karakter.")
-    print(f"A kitalálandó ország: \n{ismeretlen}")
-
-    while elet > 0:
-        tipp = input("Adj meg egy betűt, vagy megoldást: ").strip()
-
-        if tipp.lower() == orszag.lower():
-            print("Gratulálok, kitaláltad az országot! 🎉")
-            
-            while True:
-                ask = input("Szeretnél még játszani? (Igen/Nem)")
-                if ask.lower() == "igen":
-                    jo_tippek.clear()
-                    rossz_tippek.clear()
-                    kezdes()
-                elif ask.lower() == "nem":
-                    print("Játék vége!")
-                    exit()
-                else:
-                    print("Ezt nem tudom értelmezni")
-                
-
-        if "_" not in ismeretlen:
-            print("Gratulálok, kitaláltad az országot! 🎉")
-            while True:
-                ask = input("Szeretnél még játszani? (Igen/Nem)")
-                if ask.lower() == "igen":
-                    jo_tippek.clear()
-                    rossz_tippek.clear()
-                    kezdes()
-                elif ask.lower() == "nem":
-                    print("Játék vége!")
-                    exit()
-                else:
-                    print("Ezt nem tudom értelmezni")
-
-        #már felhasznált betű
-        elif tipp.lower() in jo_tippek or tipp.lower() in rossz_tippek:
-            print("Ezt a betűt már próbáltad! ❌")
-
-        #kilépés
-        elif tipp.lower() == "quit":
-            kilepes = input("- Kilépés -\nBiztos ki akarsz lépni? (igen/nem): ").strip().lower()
-            if kilepes == "igen":
-                print("Kilépés..")
-                exit()
-            else:
-                print("Játék folytatása...")
-                continue
-
-        #helyes válasz
-        elif tipp.lower() in orszag.lower():
-            jo_tippek.append(tipp.lower())
-            print(f"Helyes válasz! ✅")
-            print("> Rossz válaszok:", ", ".join(rossz_tippek), "\n> Jó válaszok:", ", ".join(jo_tippek))
-
-            #megadott betűk kicserélése
-            uj_ismeretlen = ""
-            for i in range(len(orszag)):
-                if orszag[i].lower() == tipp.lower():
-                    uj_ismeretlen += orszag[i] + " "
-                else:
-                    uj_ismeretlen += ismeretlen[i * 2] + " "
-            ismeretlen = uj_ismeretlen
-            print(ismeretlen)
-
         #rossz válasz
         elif tipp.lower() not in orszag.lower():
             rossz_tippek.append(tipp.lower())
@@ -321,19 +119,18 @@ def hard():
             print("> Rossz válaszok:", ", ".join(rossz_tippek), "\n> Jó válaszok:", ", ".join(jo_tippek))
             print("> Megmaradt életed:", elet, " 💔")
 
-            if elet == 0:
-                print(f"Vesztettél! Az ország: {orszag}")
-                while True:
-                    ask = input("Szeretnéd újra megpróbálni? (Igen/Nem)")
-                    if ask.lower() == "igen":
-                        jo_tippek.clear()
-                        rossz_tippek.clear()
-                        break
-                    elif ask.lower() == "nem":
-                        print("Játék vége!")
-                        exit
-                    else:
-                        print("Ezt nem tudom értelmezni! Igen vagy Nem választ fogadok el csak.")
+    print(f"Vesztettél! Az ország: {orszag}")
+
+    while True:
+        ask = input("Szeretnéd újra megpróbálni? (Igen/Nem)")
+        if ask.lower() == "igen":
+            kezdes()
+        elif ask.lower() == "nem":
+            print("Játék vége!")
+            exit()
+        else:
+            print("Ezt nem tudom értelmezni! Igen vagy Nem választ fogadok el csak.")
+
 
 #Nehézség kiválasztása
 def kezdes():
@@ -342,15 +139,15 @@ def kezdes():
             jatek_valasztas = int(input("Válassz szintet!\nKönnyű (1)\nKözepes (2)\nNehéz (3)\nVálassz!: "))
             if jatek_valasztas == 1:
                 print("Könnyű nehézség kiválasztva! ✅")
-                easy()
+                jatek(0)
                 break
             elif jatek_valasztas == 2:
                 print("Közepes nehézség kiválasztva! ✅")
-                medium()
+                jatek(1)
                 break
             elif jatek_valasztas == 3:
                 print("Nehéz nehézség kiválasztva! ✅")
-                hard()
+                jatek(2)
                 break
             else:
                 print("Helytelen formátum! ❌")
